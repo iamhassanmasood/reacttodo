419f7560e437ce0bf91f92b96ce0e146db4410f4
Hassan Masood <iamhassanmasood@gmail.com>
2019-04-09 04:13:00 +0500

    form creat

--- practiceproject/src/components/createTodoComponent.js
+++ practiceproject/src/components/createTodoComponent.js
@@ -53,54 +53,7 @@ export default class CreateTodo extends Component {
               onChange={this.todoDescriptionHandler}
             />
           </div>
-          <div className="form-group">
-            <label>Responsible: </label>
-            <input
-              className="form-check-input"
-              type="text"
-              className="form-control"
-              value={this.state.todoResponsible}
-              onChange={this.todoResponsibleHandler}
-            />
-          </div>
-          <div className="form-group">
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="priorityOptions"
-                id="low"
-                value="low"
-                checked={this.state.todoPriority === "low"}
-                onChange={this.todoPriorityHandler}
-              />
-              <label> Low </label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="priorityOptions"
-                id="medium"
-                value="medium"
-                checked={this.state.todoPriority === "medium"}
-                onChange={this.todoPriorityHandler}
-              />
-              <label> Medium </label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="priorityOptions"
-                id="high"
-                value="high"
-                checked={this.state.todoPriority === "high"}
-                onChange={this.todoPriorityHandler}
-              />
-              <label> High </label>
-            </div>
-          </div>
+
         </form>
       </div>
     )
