568c73cdb0d20fc4df846d0b09e443fed38ec4a9
Hassan Masood <iamhassanmasood@gmail.com>
2019-04-09 04:11:26 +0500

    form

--- practiceproject/src/components/createTodoComponent.js
+++ practiceproject/src/components/createTodoComponent.js
@@ -2,57 +2,106 @@ import React, { Component } from 'react'
 
 export default class CreateTodo extends Component {
 
-constructor(){
+  constructor() {
     super();
 
     this.todoDescriptionHandler = this.todoDescriptionHandler.bind(this);
     this.todoResponsibleHandler = this.todoResponsibleHandler.bind(this);
     this.todoPriorityHandler = this.todoPriorityHandler.bind(this);
-    this.onSubmitTodo= this.onSubmitTodo.bind(this);
-  this.state = {
-        todoDescription:"",
-        todoResponsible:"",
-        todoPriority:"",
-        todoCompleted:false
+    this.onSubmitTodo = this.onSubmitTodo.bind(this);
+    this.state = {
+      todoDescription: "",
+      todoResponsible: "",
+      todoPriority: "",
+      todoCompleted: false
     }
-}
-todoDescriptionHandler= (event)=>{
-  this.setState({
-    todoDescription: event.target.value,
-  })
-}
+  }
+  todoDescriptionHandler = (event) => {
+    this.setState({
+      todoDescription: event.target.value,
+    })
+  }
 
-todoPriorityHandler= (event)=>{
-  this.setState({
-    todoPriority: event.target.value,
-  })
-}
+  todoPriorityHandler = (event) => {
+    this.setState({
+      todoPriority: event.target.value,
+    })
+  }
 
-todoResponsibleHandler= (event) => {
-  this.setState({
-    todoResponsible: event.target.value,
-  })
-}
+  todoResponsibleHandler = (event) => {
+    this.setState({
+      todoResponsible: event.target.value,
+    })
+  }
 
-onSubmitTodo = (e) => {
-  e.preventDefault();
-}
+  onSubmitTodo = (e) => {
+    e.preventDefault();
+  }
 
   render() {
     return (
-      <div style ={{marginTop:20}}>
+      <div style={{ marginTop: 20 }}>
         <h3>Create New ToDo</h3>
-        <div className="form-group">        
-        <form onSubmit = {this.onSubmitTodo}>
-          <label>Description: </label>
-          <input
-            type="text"
-            className="form-control"
-            value={this.state.todoDescription}
-            onChange={this.todoDescriptionHandler}
-          />
+        <form onSubmit={this.onSubmitTodo}>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              className="form-check-input"
+              type="text"
+              className="form-control"
+              value={this.state.todoDescription}
+              onChange={this.todoDescriptionHandler}
+            />
+          </div>
+          <div className="form-group">
+            <label>Responsible: </label>
+            <input
+              className="form-check-input"
+              type="text"
+              className="form-control"
+              value={this.state.todoResponsible}
+              onChange={this.todoResponsibleHandler}
+            />
+          </div>
+          <div className="form-group">
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="low"
+                value="low"
+                checked={this.state.todoPriority === "low"}
+                onChange={this.todoPriorityHandler}
+              />
+              <label> Low </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="medium"
+                value="medium"
+                checked={this.state.todoPriority === "medium"}
+                onChange={this.todoPriorityHandler}
+              />
+              <label> Medium </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="high"
+                value="high"
+                checked={this.state.todoPriority === "high"}
+                onChange={this.todoPriorityHandler}
+              />
+              <label> High </label>
+            </div>
+          </div>
         </form>
-        </div>
       </div>
     )
   }
