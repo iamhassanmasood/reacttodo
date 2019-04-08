5320ee43a09ee3fa7e7ca875c9d2a530dc118383
Hassan Masood <iamhassanmasood@gmail.com>
2019-04-09 03:27:43 +0500

    description form

--- practiceproject/src/components/createTodoComponent.js
+++ practiceproject/src/components/createTodoComponent.js
@@ -7,7 +7,7 @@ constructor(){
 
     this.todoDescriptionHandler = this.todoDescriptionHandler.bind(this);
     this.todoResponsibleHandler = this.todoResponsibleHandler.bind(this);
-    this.todopriorityHandler = this.todopriorityHandler.bind(this);
+    this.todoPriorityHandler = this.todoPriorityHandler.bind(this);
     this.onSubmitTodo= this.onSubmitTodo.bind(this);
   this.state = {
         todoDescription:"",
@@ -40,8 +40,19 @@ onSubmitTodo = (e) => {
 
   render() {
     return (
-      <div>
-        <p>Create Todo</p>
+      <div style ={{marginTop:20}}>
+        <h3>Create New ToDo</h3>
+        <div className="form-group">        
+        <form onSubmit = {this.onSubmitTodo}>
+          <label>Description: </label>
+          <input
+            type="text"
+            className="form-control"
+            value={this.state.todoDescription}
+            onChange={this.todoDescriptionHandler}
+          />
+        </form>
+        </div>
       </div>
     )
   }
